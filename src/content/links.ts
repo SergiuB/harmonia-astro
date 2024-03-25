6d3575f9fdf6cdddd6abab6024489a8e4a3c7913
SergiuB <buciuc_sergiu@yahoo.com>
2024-03-25 18:29:38 +0200

    complete homepage

---
+++ src/content/links.ts
@@ -0,0 +1,26 @@
+export const links = [
+  {
+    title: "Facilitatori",
+    url: "/",
+  },
+  {
+    title: "Activitati",
+    url: "/",
+  },
+  {
+    title: "Evenimente",
+    url: "/",
+  },
+  {
+    title: "Membership",
+    url: "/",
+  },
+  {
+    title: "Hub",
+    url: "/",
+  },
+  {
+    title: "Contact",
+    url: "/",
+  },
+];
