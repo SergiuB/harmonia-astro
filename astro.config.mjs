8038e6a654927e529608c856b96d6fcd4168b7cb
SergiuB <buciuc_sergiu@yahoo.com>
2024-07-03 16:56:59 +0300

    social icons and links in footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs()],
+  integrations: [icon(), tailwind(), alpinejs()],
 });
--- src/content/links.ts
+++ src/content/links.ts
@@ -17,6 +17,6 @@ export const links = [
   spaceLink,
   {
     title: "Contact",
-    url: "/contact",
+    url: "https://wa.link/0y6cpq",
   },
 ];
