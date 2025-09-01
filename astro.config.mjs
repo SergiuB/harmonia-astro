568eb0f40176c9af4e3f87ba1875bcacde2a7724
SergiuB <buciuc_sergiu@yahoo.com>
2025-09-01 18:19:00 +0300

    refer loading the CalendarEvents component (server island)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
 import icon from "astro-icon";
+import netlify from "@astrojs/netlify"; // or '/edge' depending on target
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [icon(), tailwind(), alpinejs()],
+  adapter: netlify(),
 });
